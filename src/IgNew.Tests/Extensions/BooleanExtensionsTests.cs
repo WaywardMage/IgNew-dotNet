0a1655092475cd241b1b0ae41a9784d006f0d22c
Tanner Watson <tanner@ignew.com>
2012-06-01 14:12:57 -0400

    Migrated Boolean extensions and wrote unit tests

---
+++ src/IgNew.Tests/Extensions/BooleanExtensionsTests.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BooleanExtensionsTests.cs" company="IgNew LLC">
+//   Copywright (c) 2012 IgNew LLC. All rights reserved.
+// </copyright>
+// <summary>
+//   Unit tests for <see cref="BooleanExtensions" />.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace IgNew.Extensions.Tests
+{
+    #region Using Directives
+
+    using IgNew.Extensions;
+
+    using NUnit.Framework;
+
+    #endregion
+
+    /// <summary>
+    /// Unit tests for <see cref="BooleanExtensions"/>.
+    /// </summary>
+    [TestFixture]
+    public class BooleanExtensionsTests
+    {
+        #region Methods
+
+        #region ToString
+
+        [Test]
+        public void Test_ToString_true()
+        {
+            var trueString = "true";
+            var falseString = "false";
+
+            Assert.That(true.ToString(trueString, falseString), Is.EqualTo(trueString));
+        }
+
+        [Test]
+        public void Test_ToString_false()
+        {
+            var trueString = "true";
+            var falseString = "false";
+
+            Assert.That(false.ToString(trueString, falseString), Is.EqualTo(falseString));
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ src/IgNew/Extensions/BooleanExtensions.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BooleanExtensions.cs" company="IgNew LLC">
+//   Copywright (c) 2012 IgNew LLC. All rights reserved.
+// </copyright>
+// <summary>
+//   <see cref="Boolean" /> extension methods.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace IgNew.Extensions
+{
+    #region Using Directives
+
+    using System;
+
+    #endregion
+
+    /// <summary>
+    /// <see cref="Boolean"/> extension methods.
+    /// </summary>
+    public static class BooleanExtensions
+    {
+        #region Method
+
+        /// <summary>
+        /// Allows for representation of the boolean as a string pair
+        /// </summary>
+        /// <example>
+        /// <code>
+        ///     bool myBool = true;
+        ///     Console.WriteLine(myBool.ToString("yes", "no"));
+        ///     Console.WriteLine(myBool.ToString("Y", "N"));
+        ///     Console.WriteLine(myBool.ToString("On", "Off"));
+        /// </code>
+        /// </example>
+        /// <param name="target">Object being extended.</param>
+        /// <param name="trueString">True string representation.</param>
+        /// <param name="falseString">False string representation.</param>
+        /// <returns><paramref name="trueString"/> if true, <paramref name="falseString"/> otherwise.</returns>
+        public static string ToString(this bool target, string trueString, string falseString)
+        {
+            return target ? trueString : falseString;
+        }
+
+        #endregion
+    }
+}
